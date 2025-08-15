3f227b5069f00aa1e0b9f060512f73fa5d126d81
GitHub Action <action@github.com>
2025-08-15 23:02:06 +0000

    Template cleanup

--- src/main/kotlin/com/github/boykore/kanawagaintellijcolortheme/services/MyProjectService.kt
+++ src/main/kotlin/com/github/boykore/kanawagaintellijcolortheme/services/MyProjectService.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.services
+package com.github.boykore.kanawagaintellijcolortheme.services
 
 import com.intellij.openapi.components.Service
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.boykore.kanawagaintellijcolortheme.MyBundle
 
 @Service(Service.Level.PROJECT)
 class MyProjectService(project: Project) {
